091478409c3bb8580525d498a08856a82d15cb28
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-04-09 17:51:27 -0700

    User Profile

--- Chapter 4/bookmarks/bookmarks/settings.py
+++ Chapter 4/bookmarks/bookmarks/settings.py
@@ -126,3 +126,8 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
