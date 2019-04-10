1a2ae20dcd1d59c9836d35868c4564c9f7315e6f
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-04-09 20:41:39 -0700

    Profile by User

--- Chapter 4/bookmarks/account/admin.py
+++ Chapter 4/bookmarks/account/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Profile
+from .models import	Profile
 
-# Register your models here.
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-	list_display = ['user', 'date_of_birth', 'photo']
+	list_display =	['user', 'date_of_birth', 'photo']
+
+# Register your models here.
+
+
 
 
 
--- Chapter 4/bookmarks/account/urls.py
+++ Chapter 4/bookmarks/account/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
 	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 	path('register/', views.register, name='register' ),
+	path('edit/', views.edit, name='edit'),
 
 	
 	
