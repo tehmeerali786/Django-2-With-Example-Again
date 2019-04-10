590933324e2081d0da6fab2d8b9b591208e35b8f
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-04-09 21:12:02 -0700

    messages

--- Chapter 4/bookmarks/account/views.py
+++ Chapter 4/bookmarks/account/views.py
@@ -8,6 +8,8 @@ from .forms import LoginForm, UserRegistrationForm, \
                    UserEditForm, ProfileEditForm
 
 
+
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -59,9 +61,13 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
 
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
 
     return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form':profile_form})
+
