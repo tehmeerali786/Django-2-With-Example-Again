defb1d0ff43eb182bdac7b48e42c4f4385032375
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-04-17 07:13:10 -0700

    Jquery

--- Chapter 5/bookmarks/bookmarks/settings.py
+++ Chapter 5/bookmarks/bookmarks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'images.apps.ImagesConfig',
+    'sorl.thumbnail',
     ]
     
 
--- Chapter 5/bookmarks/images/views.py
+++ Chapter 5/bookmarks/images/views.py
@@ -6,6 +6,9 @@ from .forms import ImageCreateForm
 from django.shortcuts import get_object_or_404
 from .models import Image
 
+from django.http import JsonResponse 
+from django.views.decorators.http import require_POST
+
 # Create your views here.
 
 @login_required 
@@ -35,3 +38,22 @@ def image_create(request):
 def image_detail(request, id, slug):
 	image = get_object_or_404(Image, id=id, slug=slug)
 	return render(request, 'images/image/detail.html', {'section':'images', 'image':image})
+
+@login_required 
+@require_POST
+def image_like(request):
+	image_id = request.POST.get('id')
+	action = request.POST.get('action')
+	if image_id and action:
+		try:
+			image = Image.Objects.get(id=image_id)
+			if action == 'like':
+				image.users_like.add(request.user)
+			else:
+				image.users_like.remove(request.user)
+			return JsonResponse({'status': 'ok'})
+
+		except:
+			pass
+
+		return JsonResponse({'status' : 'ko'})
